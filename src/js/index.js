2509429387d9e3c9ddeb150fea7795b8d681a70a
otaviogrocha <101598593+otaviogrocha@users.noreply.github.com>
2022-03-14 17:05:16 -0300

    commit inicial

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+/*
+quando clicar no pokemon ocultar o outro na listagem
+
+1- listagem
+2- cartao pokemon
+
+crir 2 var para os elementos na tela
+
+remover a classe aberto(display block)
+
+evento de clique
+
+id para cada pokemon para identificar qual cartao mostrar
+
+remover a classe ativo na listagem
+*/
+
+const listagem = document.querySelectorAll('.pokemon')
+const cartaoPokemon = document.querySelectorAll('.cartao_pokemon') 
+
+//evento de clique
+listagem.forEach(pokemon => {
+    pokemon.addEventListener('click',() => {
+        
+        //remover a classe aberto(display block)
+       
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+
+        //id para cada pokemon para identificar qual cartao mostrar
+        
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const cartaoPokemonParaAbrir = document.getElementById('cartao_' + idPokemonSelecionado)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+        
+        //remover a classe ativo na listagem
+
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+        //adicionar a classe ativo na listagem
+
+         const pokemonselecionadonaLista = document.getElementById(idPokemonSelecionado)
+         pokemonselecionadonaLista.classList.add('ativo')
+
+    })
+})
